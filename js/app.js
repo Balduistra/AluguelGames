ef37a57b9dee65d1a268e35936a7d11c7fc20ea3
Balduistra <ppereirab.jr@gmail.com>
2024-03-10 08:56:51 -0300

    aluguel de games

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function alterarStatus (inidice) {
+    let tagA = document.getElementsByTagName('a')
+    let botao = tagA[inidice];
+
+    if (botao.)
+    botao.classList.add('dashboard__item__button--return');
+    
+}
+
